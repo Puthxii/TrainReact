cb71fd17a691b1663f6318e7c7d7f020212e07fd
Puth1998 <Teenaputh12@hotmail.com>
2019-08-31 15:18:21 +0700

    +loading

--- src/App.js
+++ src/App.js
@@ -3,27 +3,35 @@ import axios from 'axios';
 
 class App extends Component{
   state = {
-    users : []
+    users : [],
+    loading: true
   }
   
   componentDidMount(){
-    axios('https://api.randomuser.me/?nat=US&results=5')
-    .then(response => this.setState({users: response.data.results})); 
+    setTimeout(() =>{
+      axios('https://api.randomuser.me/?nat=US&results=5')
+      .then(response => {
+        this.setState({
+          users: response.data.results,
+          loading:  false
+        })
+      })
+    },10)
+   
   }
-
-
   
   render(){
     return(
       <div>
-        { this.state.users.map(user => (
+        { (!this.state.loading) ? this.state.users.map(user => 
           <div>
               <div>{user.name.first}</div>
               <div>{user.email}</div>
               <div>{user.phone}</div>
               <hr />
           </div>       
-        ))}
+        ) : "Loading..."
+        }
       </div>
     )
   }
